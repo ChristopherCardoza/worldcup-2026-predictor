74c5fe33211c6ed65004e41cb2ccd599a968ee9c
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-01 23:31:05 -0400

    Namespace formatting fixed and GroupTeam.cs file added

--- backend/WorldCupPredictor.API/Models/Fixture.cs
+++ backend/WorldCupPredictor.API/Models/Fixture.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 
-namespace WorldCupPredictor.API.Models;
-
-/// <summary>
-/// Represents a fixture in the tournament
-/// </summary>
-public class Fixture
+namespace WorldCupPredictor.API.Models
 {
-	
+    /// <summary>
+    /// Represents a fixture in the tournament
+    /// </summary>
+    public class Fixture
+    {
+
+    }
+
 }
+
+
--- backend/WorldCupPredictor.API/Models/Group.cs
+++ backend/WorldCupPredictor.API/Models/Group.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 
-namespace WorldCupPredictor.API.Models;
-
-/// <summary>
-/// Represents a group in the World Cup
-/// </summary>
-public class Group
+namespace WorldCupPredictor.API.Models
 {
-	public int Id { get; set; }
-	public int TournamentId { get; set; }
-	public string Name { get; set; } = "";
-	public Tournament tournament { get; set; } = null!;
-	// needs groupteam list
+    /// <summary>
+    /// Represents a group in the World Cup
+    /// </summary>
+    public class Group
+    {
+        public int Id { get; set; }
+        public int TournamentId { get; set; }
+        public string Name { get; set; } = "";
+        public Tournament tournament { get; set; } = null!;
+        public List<GroupTeam> GroupTeams { get; set; } = [];
+
+    }
 
 }
+
