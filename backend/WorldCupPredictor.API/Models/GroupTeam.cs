732fdc9de9d38802241572c636d20f26f40b05b7
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-01 23:34:00 -0400

    GroupTeam.cs Model completed

--- backend/WorldCupPredictor.API/Models/GroupTeam.cs
+++ backend/WorldCupPredictor.API/Models/GroupTeam.cs
@@ -1,7 +1,13 @@
 ﻿namespace WorldCupPredictor.API.Models
 {
+    /// <summary>
+    /// Represents a country in a group
+    /// </summary>
     public class GroupTeam
     {
-
+        public int GroupId { get; set; }
+        public int CountryId { get; set; }
+        public Group Group { get; set; } = null!;
+        public Country Country { get; set; } = null!;
     }
 }
