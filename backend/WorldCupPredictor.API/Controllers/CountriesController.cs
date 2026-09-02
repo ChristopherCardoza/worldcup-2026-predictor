2cdc49abb7ea2dca4adb57a610c53e36598960cd
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-02 00:19:21 -0400

    Simple endpoint added, UpdateModel Migrations, running now  and shows seeded countries

---
+++ backend/WorldCupPredictor.API/Controllers/CountriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldCupPredictor.API.Data;
+
+namespace WorldCupPredictor.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public CountriesController(AppDbContext db) => _db = db;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+        => Ok(await _db.Countries.OrderBy(c => c.Name).ToListAsync());
+}
+
+
---
+++ backend/WorldCupPredictor.API/Migrations/20260902041613_UpdateModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorldCupPredictor.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "flagPath",
+                table: "Countries",
+                newName: "FlagPath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FlagPath",
+                table: "Countries",
+                newName: "flagPath");
+        }
+    }
+}
