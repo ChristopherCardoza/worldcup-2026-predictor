0d84e12d3864d3358c0dedf96fdb9c101ea6885a
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-01 23:06:37 -0400

    Country Model Class added

---
+++ backend/WorldCupPredictor.API/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Represents a country in the World Cup.
+/// </summary>
+public class Country
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = "";
+
+	public string IsoCode { get; set; } = "";
+
+	public string flagPath { get; set; } = "";
+
+	public int? OppCode { get; set; }
+
+
+}
