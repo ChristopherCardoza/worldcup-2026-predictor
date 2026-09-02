cfca5d5b55f184b59fc62772fe2514d3bad204c4
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-01 23:41:29 -0400

    GroupStanding.cs Model completed

---
+++ backend/WorldCupPredictor.API/Models/GroupStanding.cs
@@ -0,0 +1,20 @@
+﻿namespace WorldCupPredictor.API.Models
+{
+    public class GroupStanding
+    {
+        public int SimulationRunId { get; set; }
+        public int GroupId { get; set; }
+        public int CountryId { get; set; }
+        public int Played { get; set; }
+        public int Won { get; set; }
+        public int Drawn { get; set; }
+        public int Lost { get; set; }
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+        public int Points { get; set; }
+        public int Rank { get; set; }
+        public SimulationRun SimulationRun { get; set; } = null!;
+        public Group Group { get; set; } = null!;
+        public Country Country { get; set; } = null!;
+    }
+}
