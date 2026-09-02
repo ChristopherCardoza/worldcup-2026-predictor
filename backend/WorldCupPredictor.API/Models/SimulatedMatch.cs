c3af688c979c79338ee05d806b7e9c98822161a7
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-01 23:38:49 -0400

    SimulatedRun and SimulatedMatch classes added to Models

---
+++ backend/WorldCupPredictor.API/Models/SimulatedMatch.cs
@@ -0,0 +1,15 @@
+﻿namespace WorldCupPredictor.API.Models
+{
+    public class SimulatedMatch
+    {
+        public int Id { get; set; }
+        public int SimulationRunId { get; set; }
+        public int FixtureId { get; set; }
+        public double TeamAWinProbability { get; set; }
+        public int GoalsA { get; set; }
+        public int GoalsB { get; set; }
+        public string Outcome { get; set; } = ""; // TeamAWin, Draw, TeamBWin
+        public SimulationRun SimulationRun { get; set; } = null!;
+        public Fixture Fixture { get; set; } = null!;
+    }
+}
---
+++ backend/WorldCupPredictor.API/Models/SimulationRun.cs
@@ -0,0 +1,14 @@
+﻿namespace WorldCupPredictor.API.Models
+{
+    public class SimulationRun
+    {
+        public int Id { get; set; }
+        public int TournamentId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "InProgress"; // going to be GroupComplete, etc
+        public int? ChampionCountryId { get; set; }
+        public Tournament Tournament { get; set; } = null!;
+        public Country? ChampionCountry { get; set; }
+        public List<SimulatedMatch> SimulatedMatches { get; set; } = [];
+    }
+}
