24d48ce84ff8899655919641d18dda610ce88867
ChristopherCardoza <cardozachristopher1@gmail.com>
2026-09-07 00:08:08 -0400

    ML DTOs made in Services folder

---
+++ backend/WorldCupPredictor.API/Services/Ml/MlPredictRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace WorldCupPredictor.API.Services.Ml
+{
+    public class MlPredictRequest
+    {
+        public string team { get; set; } = "";
+        public string opponent { get; set; } = "";
+        public string venue { get; set; } = "Neutral";
+        public string? match_date { get; set; }
+    }
+
+    public class MlPredictResponse
+    {
+        public string team { get; set; } = "";
+        public string opponent { get; set; } = "";
+        public string venue { get; set; } = "";
+        public double win_probability { get; set; }
+        public bool predicted_win { get; set; }
+    }
+}
